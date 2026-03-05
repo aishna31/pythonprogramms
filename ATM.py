47dc14e618f9fd311c7991da89c02f4ccc286502
aishna31 <aishnavatyani08@gmail.com>
2026-03-05 18:20:01 +0530

    atm final

--- ATM.py
+++ ATM.py
@@ -22,7 +22,7 @@ if (userpin==pin):
         e=open("atm.txt","r")
         checkbalance=e.read()
         e.close()
-        print(checkbalance)
+        print( checkbalance)
     elif(task=="2"):
         depositeamount=int(input("please enter the amount you need to deposite"))
         f=open("atm.txt","r")
@@ -30,8 +30,13 @@ if (userpin==pin):
         f.close()
         accbalance= int(accbalance.split("=")[1])
         newdepositebalance=accbalance+depositeamount
+        newdepositebalance=str(newdepositebalance)
         print(depositeamount,"is successfully added to you account")
         print("the current balance in your acc after updation is ",newdepositebalance)
+        k="account balance = "+newdepositebalance
+        h=open("atm.txt","w")
+        h.write(k)
+        h.close()
     elif(task=="depositemoney"):
         depositeamount=int(input("please enter the amount you need to deposite"))
         f=open("atm.txt","r")
@@ -39,8 +44,13 @@ if (userpin==pin):
         f.close()
         accbalance= int(accbalance.split("=")[1])
         newdepositebalance=accbalance+depositeamount
+        newdepositebalance=str(newdepositebalance)
         print(depositeamount,"is successfully added to you account")
         print("the current balance in your acc after updation is ",newdepositebalance)
+        k="account balance = "+newdepositebalance
+        h=open("atm.txt","w")
+        h.write(k)
+        h.close()
     elif(task=="deposite money"):
             depositeamount=int(input("please enter the amount you need to deposite"))
             f=open("atm.txt","r")
@@ -48,8 +58,51 @@ if (userpin==pin):
             f.close()
             accbalance= int(accbalance.split("=")[1])
             newdepositebalance=accbalance+depositeamount
+            newdepositebalance=str(newdepositebalance)
             print(depositeamount,"is successfully added to you account")
-            print("the  current balance in your acc after updation is ",newdepositebalance)
-
+            print("the current balance in your acc after updation is ",newdepositebalance)
+            k="account balance = "+newdepositebalance
+            h=open("atm.txt","w")
+            h.write(k)
+            h.close()
+    elif(task=="3"):
+         withdrawamount=int(input("please enter the amount you have to withdraw"))
+         g=open("atm.txt","r")
+         accbalance=g.read()
+         g.close()
+         accbalance= int(accbalance.split("=")[1])
+         withdrawbalance=accbalance-withdrawamount
+         withdrawbalance=str(withdrawbalance)
+         print("the current balance in your account is",withdrawbalance)
+         m="account balance = "+ withdrawbalance
+         i=open("atm.txt","w")
+         i.write( m)
+         i.close()
+    elif(task=="withdrawmoney"):
+         withdrawamount=int(input("please enter the amount you have to withdraw"))
+         g=open("atm.txt","r")
+         accbalance=g.read()
+         g.close()
+         accbalance= int(accbalance.split("=")[1])
+         withdrawbalance=accbalance-withdrawamount
+         withdrawbalance=str(withdrawbalance)
+         print("the current balance in your account is",withdrawbalance)
+         m="account balance = "+ withdrawbalance
+         i=open("atm.txt","w")
+         i.write( m)
+         i.close()
+    elif(task=="withdraw money"):
+         withdrawamount=int(input("please enter the amount you have to withdraw"))
+         g=open("atm.txt","r")
+         accbalance=g.read()
+         g.close()
+         accbalance= int(accbalance.split("=")[1])
+         withdrawbalance=accbalance-withdrawamount
+         withdrawbalance=str(withdrawbalance)
+         print("the current balance in your account is",withdrawbalance)
+         m="account balance = "+ withdrawbalance
+         i=open("atm.txt","w")
+         i.write( m)
+         i.close()
 else:
     print("incorrect pin")    
