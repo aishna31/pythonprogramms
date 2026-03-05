3aaf926819f070b4e67dac8b869cf015c8c574ed
aishna31 <aishnavatyani08@gmail.com>
2026-03-05 18:37:16 +0530

    moviesdictnew

--- moviesdic.py
+++ moviesdic.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+# movies
 movies = [
     {
         "name":"entertainment",
@@ -33,42 +33,3 @@ for i in range(0,len(movies)):
              print(movies[i]["name"])
          if(a[j]=="alia bhatt"):
            print(movies[i]["name"])       
-               
-=======
-movies = [
-    {
-        "name":"entertainment",
-        "year":2010,
-        "actors":["akshay","tamnaah","sonu"]
-    },
-    {
-        "name":"ishq",
-        "year":2001,
-        "actors":["ajay","aamir","kajol"]
-    },
-    {
-        "name":"drishyam",
-        "year":2017,
-        "actors":["ajay devgan","tabu","aishna vatyani"]
-    },
-    {
-        "name":"freddy",
-        "year":2020,
-        "actors":["kartik aryan","kirti sanon","alia bhatt"]
-    },
-    {
-        "name":"bhool bhuliya",
-        "year":2015,
-        "actors":["akshay kumar","vidhya balan","amisha patel"]
-    }    
-]
-for i in range(0,len(movies)):
-    a=movies[i]["actors"]
-    for j in range(0,len(a)):
-         if(a[j]=="akshay kumar"):
-             print(movies[i]["name"])
-         if(a[j]=="alia bhatt"):
-           print(movies[i]["name"])       
-               
->>>>>>> 3dffd07cfde143da69f20387972a923981707b44
-          
