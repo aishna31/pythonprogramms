d9835c5fbc29645d939167570b9ab134bc7c9dd9
aishna31 <aishnavatyani08@gmail.com>
2026-03-05 18:40:04 +0530

    movie and negative

--- negativearray.py
+++ negativearray.py
@@ -1,17 +1,7 @@
-<<<<<<< HEAD
-
+# negative array
 x=[-1,-2,-3,-4,-5,-6,-7,1,2,-9]
 negative_x=[]
 for i in x:
     if (i<0):
         negative_x.append(i)
 print(len(negative_x))     
-=======
-
-x=[-1,-2,-3,-4,-5,-6,-7,1,2,-9]
-negative_x=[]
-for i in x:
-    if (i<0):
-        negative_x.append(i)
-print(len(negative_x))     
->>>>>>> 3dffd07cfde143da69f20387972a923981707b44
