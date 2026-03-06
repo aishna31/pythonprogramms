c762bf64c9d02504c5985a6b669fc8bb8e7e0db0
aishna31 <aishnavatyani08@gmail.com>
2026-03-06 17:51:12 +0530

    araaay

--- arrayyyy.py
+++ arrayyyy.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 a=[8,12,31,16,23,48]
 b=[]
@@ -7,14 +6,3 @@ for i in range(0,len(a)):
     inx=len_a-1-i
     c=a[inx]
     b.append(c)
-=======
-
-a=[8,12,31,16,23,48]
-b=[]
-len_a=len(a)
-for i in range(0,len(a)):
-    inx=len_a-1-i
-    c=a[inx]
-    b.append(c)
->>>>>>> 3dffd07cfde143da69f20387972a923981707b44
-print(b)    
